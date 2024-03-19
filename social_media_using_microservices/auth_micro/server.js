bdc81966a4c3ff1aef8cb4e7855241a7d8bb5a0e
Ayush Singh <aayush.singh2100@gmail.com>
2024-03-20 00:48:06 +0530

    Added cors, also added some middlewares

--- social_media_using_microservices/auth_micro/server.js
+++ social_media_using_microservices/auth_micro/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.get("/", (req, res) => {
     return res.json({message: "it's working..."});
