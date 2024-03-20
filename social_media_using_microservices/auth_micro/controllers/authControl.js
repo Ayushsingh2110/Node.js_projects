7b409d66d2f516ac3562c9bbb9e55c6f716f771d
Ayush Singh <aayush.singh2100@gmail.com>
2024-03-20 16:30:59 +0530

    Wrote registration logic, installed bcrypt

--- social_media_using_microservices/auth_micro/controllers/authControl.js
+++ social_media_using_microservices/auth_micro/controllers/authControl.js
@@ -1,7 +1,23 @@
+import bcrypt from "bcrypt";
+import prisma from "../config/db.config.js";
+
 class AuthController{
     static async register(req, res) {
-        const payload = req.body;
-        return res.json({ payload });
+        
+        try{
+            const payload = req.body;
+        const salt = bcrypt.genSaltSync(10);
+            payload.password = bcrypt.hashSync(payload.password, salt)
+            const user = await prisma.User.create({
+                data: payload
+            })
+
+            return res.status(200).json({ message: "registration successfull !!", user});
+        }catch(error){
+            return res.status(500).json({message: "something went wrong !!", error})
+        }
+
+        
     }
 }
 
